09a1f38d4a6c52cece2f86d1f7d0201dcf2ed039
srmephisto <goethesrmephisto@gmail.com>
2017-09-11 18:53:34 -0600

    corregido

--- sitio/settings.py
+++ sitio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'acm*(xr&j)eo4kvw@#t!pb(!12p4h6&swn&++3fuyeevoidav1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '<your_username>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'goethesrmephisto.pythonanywhere.com']
 
 
 # Application definition
